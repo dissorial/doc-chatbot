9747432af813f227f1581cb88b68ea05e0b06d1e
dissorial <87335884+dissorial@users.noreply.github.com>
2023-05-09 16:44:22 +0200

    [namespace] prop fix

--- pages/namespace/[namespace].tsx
+++ pages/namespace/[namespace].tsx
@@ -1,13 +1,8 @@
-// pages/[namespace].tsx
 import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
 import Home from '..';
 
 const NamespacePage: NextPage = () => {
-  const router = useRouter();
-  const { namespace } = router.query;
-
-  return <Home initialNamespace={namespace as string} />;
+  return <Home />;
 };
 
 export default NamespacePage;
