038953708f050b13db97c3655b38e28f5d1870f9
dissorial <87335884+dissorial@users.noreply.github.com>
2023-06-05 16:03:02 +0200

    deployment bug fix (?)

--- libs/localStorageKeys.ts
+++ libs/localStorageKeys.ts
@@ -1,7 +1,12 @@
 export const setItem = (key: string, value: string) => {
-  window.localStorage.setItem(key, value);
+  if (typeof window !== 'undefined') {
+    window.localStorage.setItem(key, value);
+  }
 };
 
 export const getItem = (key: string) => {
-  return window.localStorage.getItem(key);
+  if (typeof window !== 'undefined') {
+    return window.localStorage.getItem(key);
+  }
+  return null;
 };
